c8303a2d0b787b4435236c3bf0add5248aced255
Abragadabra <minin.saschencka@yandex.ru>
2024-10-16 21:59:50 +0500

    add lorem and beer button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
 import localFont from 'next/font/local'
 import './globals.css'
+import { Toaster } from '@/components/ui/toaster'
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -38,6 +39,7 @@ export default function RootLayout({
 				>
 					<Header />
 					<main className='px-6 mt-10 w-full'>{children}</main>
+					<Toaster />
 				</ThemeProvider>
 			</body>
 		</html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { useToast } from '@/hooks/use-toast'
+
 export default function Home() {
-	return <p></p>
+	const { toast } = useToast()
+	return (
+		<section>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex iste odit
+				odio repellendus, tempore dignissimos a hic, voluptates et ratione
+				veniam sapiente nesciunt atque ad repellat excepturi. Deserunt fugiat
+				illum voluptatum asperiores ad molestias accusamus quam earum eos
+				mollitia, ex atque sint rem facilis. Fuga minus, numquam ipsum soluta
+				cumque, neque eius architecto reiciendis fugiat explicabo aperiam
+				reprehenderit officia dolorum obcaecati officiis sunt repellendus,
+				facere quas eos ipsam asperiores optio fugit. Dicta itaque voluptatibus
+				praesentium perferendis corporis maxime iure cupiditate alias labore? A
+				corporis fugit alias expedita. Mollitia consequatur illo tempora maiores
+				dolore nostrum laborum porro. Culpa dolores nobis soluta!
+			</p>
+			<Button
+				className='mt-4'
+				variant='outline'
+				onClick={() => {
+					toast({
+						title: 'Покупка пива 🍺🤙',
+						description: 'С вас сняли 450 рублей',
+					})
+				}}
+			>
+				Кнопка (она прикольная)
+			</Button>
+		</section>
+	)
 }
