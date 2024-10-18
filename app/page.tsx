07e4373cad50ec82580a84e68341743721e26ca3
Alexander Minin <77795206+MrAbragadabra@users.noreply.github.com>
2024-10-18 23:29:49 +0500

    drop zone

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,10 @@
 
 export default function Home() {
 	return (
-		<section>
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex iste odit
-				odio repellendus, tempore dignissimos a hic, voluptates et ratione
-				veniam sapiente nesciunt atque ad repellat excepturi. Deserunt fugiat
-				illum voluptatum asperiores ad molestias accusamus quam earum eos
-				mollitia, ex atque sint rem facilis. Fuga minus, numquam ipsum soluta
-				cumque, neque eius architecto reiciendis fugiat explicabo aperiam
-				reprehenderit officia dolorum obcaecati officiis sunt repellendus,
-				facere quas eos ipsam asperiores optio fugit. Dicta itaque voluptatibus
-				praesentium perferendis corporis maxime iure cupiditate alias labore? A
-				corporis fugit alias expedita. Mollitia consequatur illo tempora maiores
-				dolore nostrum laborum porro. Culpa dolores nobis soluta!
-			</p>
+		<section className='rounded-lg border-4 border-dashed p-4 h-[85vh] flex items-center justify-center'>
+			<h1 className='block text-bold text-4xl transition-colors text-foreground/60 select-none'>
+				перетащите ваши файлы сюда
+			</h1>
 		</section>
 	)
 }
