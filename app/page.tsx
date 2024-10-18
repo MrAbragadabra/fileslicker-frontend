2f79df95409802eb6937e88d5854cf5935616126
Alexander Minin <77795206+MrAbragadabra@users.noreply.github.com>
2024-10-18 23:34:55 +0500

    open explorer on click

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
 'use client'
 
+import { useRef } from 'react'
+
 export default function Home() {
+	const fileInputRef = useRef<HTMLInputElement | null>(null)
+
+	const handleClick = () => {
+		if (fileInputRef.current) {
+			fileInputRef.current.click()
+		}
+	}
+
 	return (
-		<section className='rounded-lg border-4 border-dashed p-4 h-[85vh] flex items-center justify-center'>
-			<h1 className='block text-bold text-4xl transition-colors text-foreground/60 select-none'>
-				перетащите ваши файлы сюда
+		<section
+			onClick={handleClick}
+			className='rounded-lg border-4 border-dashed p-4 h-[85vh] flex items-center justify-center cursor-pointer'
+		>
+			<h1 className='block text-bold text-4xl transition-colors text-foreground/60 cursor-pointer'>
+				перетащите ваши файлы сюда (или просто нажмите)
 			</h1>
+
+			{/* Скрытый input для загрузки файлов */}
+			<input
+				type='file'
+				ref={fileInputRef}
+				style={{ display: 'none' }}
+				multiple
+			/>
 		</section>
 	)
 }
