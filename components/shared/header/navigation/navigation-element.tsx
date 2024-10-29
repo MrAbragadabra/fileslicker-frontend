87d2aed1bacd86958dc5670d1bd67c6231f5be02
Alexander Minin <77795206+MrAbragadabra@users.noreply.github.com>
2024-10-29 16:01:30 +0500

    wow

--- components/shared/header/navigation/navigation-element.tsx
+++ components/shared/header/navigation/navigation-element.tsx
@@ -1,29 +1,29 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 type LinkProps = {
-	type: "link";
-	href: string;
-	className?: string;
-	caption: string;
-};
+	type: 'link'
+	href: string
+	className?: string
+	caption: string
+}
 
 type TextProps = {
-	type: "text";
-	className?: string;
-	caption: string;
-};
+	type: 'text'
+	className?: string
+	caption: string
+}
 
-type Props = LinkProps | TextProps;
+type Props = LinkProps | TextProps
 
 export const NavigationElement = (props: Props) => {
 	return (
 		<>
-			{props.type === "link" ? (
+			{props.type === 'link' ? (
 				<Link
 					href={props.href}
 					className={
 						props.className ||
-						"font-bold transition-colors hover:text-foreground/80 text-foreground/60"
+						'font-bold transition-colors hover:text-foreground/80 text-foreground/60'
 					}
 				>
 					{props.caption}
@@ -32,12 +32,12 @@ export const NavigationElement = (props: Props) => {
 				<span
 					className={
 						props.className ||
-						"font-bold transition-colors hover:text-foreground/80 text-foreground/60 cursor-pointer"
+						'font-bold transition-colors hover:text-foreground/80 text-foreground/60 cursor-pointer'
 					}
 				>
 					{props.caption}
 				</span>
 			)}
 		</>
-	);
-};
+	)
+}
